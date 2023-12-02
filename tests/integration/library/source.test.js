94bc7090802c68c99a1e86f1b4019b59f168b867
Brendo Costa <brendocosta@id.uff.br>
2023-12-02 18:15:56 -0300

    Integration tests with input implementation

--- tests/integration/library/source.test.js
+++ tests/integration/library/source.test.js
@@ -1,36 +1,30 @@
 import Source from '../../../src/library/source';
-import Lexer from '../../../src/library/lexer';
-import {
-    End,
-    Not,
-} from '../../../src/library/tokens';
-
-function createLexerFromString(string) {
-    return new Lexer(new Source(string));
-}
 
 describe('Source', () => {
-    it('Lexer should be able to skip source\'s characters while they are whitespaces characters', () => {
-        const lexer = createLexerFromString('            ¬a  ');
-        expect(lexer.next()).toBeInstanceOf(Not);
-    });
+    describe('Input', () => {
+        it('Should support slicing', () => {
+            let source = new Source("a & b");
+            expect(source.input.slice).toBeInstanceOf(Function);
+        });
 
-    it('Lexer should be able to bump source', () => {
-        const lexer = createLexerFromString('            ¬a  ');
-        lexer.next();
-        expect(lexer.source.peek()).toEqual('a');
-    });
+        it('Should support slicing by two arguments', () => {
+            let source = new Source("a & b");
+            expect(source.input.slice.length).toBe(2);
+        });
 
-    it('Lexer should be able to take source\'s characters while they are alphabetic characters', () => {
-        const lexer = createLexerFromString('  somename  ');
-        expect(lexer.next().name).toEqual('somename');
-    });
+        it('Should support slicing given two arguments', () => {
+            let source = new Source("a & b");
+            expect(source.input.slice(0, 3)).toEqual("a &");
+        });
 
-    it('Lexer should return an End token after consuming all source\'s characters', () => {
-        const lexer = createLexerFromString('a & b');
-        lexer.source.peek = () => null;
-        expect(lexer.next()).toBeInstanceOf(End);
-        lexer.source.peek = () => undefined;
-        expect(lexer.next()).toBeInstanceOf(End);
+        it('Should support indexing', () => {
+            let source = new Source("a & b");
+            expect(source.input[2]).toEqual("&");
+        });
+
+        it('Should support the length determination', () => {
+            let source = new Source("a & b");
+            expect(source.input.length).toBe(5);
+        });
     });
 });
