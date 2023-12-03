e2edbc622115ec01574ec592377641e801ebd498
Isaac Ferreira <zaclummys@gmail.com>
2023-12-03 14:11:04 +0000

    Install playwright

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -9,6 +9,10 @@ export default defineConfig({
         environment: 'node',
         coverage: {
             all: true
-        }
+        },
+        include: [
+            './tests/unit/**/*.test.js',
+            './tests/integration/**/*.test.js',
+        ]
     },
 });
