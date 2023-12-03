567120018371fc6036d1c6cb67fe40d662a34798
Brendo Costa <brendocosta@id.uff.br>
2023-12-03 11:27:27 -0300

    Added string iteration tests

--- tests/integration/library/source.test.js
+++ tests/integration/library/source.test.js
@@ -26,5 +26,40 @@ describe('Source', () => {
             let source = new Source("a & b");
             expect(source.input.length).toBe(5);
         });
+
+        it('Should support iterators through the default protocol', () => {
+            let source = new Source("a & b");
+            expect(source.input[Symbol.iterator]).toBeInstanceOf(Function);
+            expect(source.input[Symbol.iterator]()).toMatchObject({
+                next: expect.any(Function)
+            });
+        });
+
+        it('Should be iterable through the default statement', () => {
+            let source = new Source("ab cdefghi jk");
+            let i = 0;
+            for (let character of source.input) {
+                expect(character).toEqual(source.input[i]);
+                i++;
+            }
+        });
+
+        /*it('Should support iteration through Unicode characters', () => {
+            let source = new Source("ab cde🍕fghi🍫jk");
+            let i = 0;
+            for (let character of source.input) {
+                expect(character).toEqual(source.input[i]);
+                i++;
+            }
+        });*/
+
+        it('Should support the well-known iterating object protocol', () => {
+            let source = new Source("a & b");
+            let iterator = source.input[Symbol.iterator]();
+            expect(iterator.next()).toMatchObject({
+                done: expect.any(Boolean),
+                value: expect.any(String)
+            });
+        });
     });
 });
